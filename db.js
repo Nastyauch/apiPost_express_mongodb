11b9c7011f4f18fc3cdcf775a0fcdd01864d009b
Nastyauch <nastya.uchaneva@gmail.com>
2020-01-09 11:20:18 +0300

    atlas db

--- db.js
+++ db.js
@@ -1,29 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/PostDB', {useNewUrlParser: true}, (err) => {
-    if (!err) {console.log('NomgoDB connection Succeeded 27017')}
+mongoose.connect('mongodb+srv://admin:admin@apicluster-qsbz5.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: false}, (err) => {
+    if (!err) {console.log('MongoDB connection Succeeded')}
     else {console.log('Error in DB connection: ' + err)}
 });
 require('./models/posts');
 require('./models/users');
-//let MongoClient = require('mongodb').MongoClient;
-//
-//let state = {
-//    db: null
-//};
-//
-//exports.connect = function (url, done) {
-//    if (state.db) {
-//        return done();
-//    }
-//    
-//    MongoClient.connect(url, function (err, db) {
-//        if (err) {
-//            return done(err);
-//        }
-//        state.db = db;
-//        done();
-//    })
-//}
-// exports.get = function () {
-//     return state.db;
-// }
